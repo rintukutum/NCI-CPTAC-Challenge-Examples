7105bdb93d581afcac322c539dcd5a60d0ad1a10
thomasyu888 <thomasyu888@gmail.com>
2017-06-12 17:07:26 -0700

    Correct file being written

--- sc2/DRY_RUN_02_prot_prediction.R
+++ sc2/DRY_RUN_02_prot_prediction.R
@@ -1,25 +1,24 @@
-path <- ""
+path <- "sc2/"
 
 # load saved models
 load(paste0(path,"model_storage/SAVE_WEIGHT_HGSC_PROT_RNA_fold10_20ite.Rdata"))
 
 # load testing data
-HGSC_rna_EVAL  <- read.csv(paste0(path,"evaluation_data/pros_ova_rna_seq_sort_common_gene_15632.txt") , row.names= 1)
-HGSC_prot_EVAL <- read.csv(paste0(path,"evaluation_data/pros_ova_proteome_sort_common_gene_6577.txt"), row.names= 1)
+HGSC_rna_EVAL  <- read.csv(paste0(path,"evaluation_data/pros_ova_rna_seq_sort_common_gene_15632.txt"), row.names= 1, sep="\t",check.names = F)
+HGSC_prot_EVAL <- read.csv(paste0(path,"evaluation_data/pros_ova_proteome_sort_common_gene_6577.txt"), row.names= 1, sep="\t",check.names = F)
 
 # take common proteins and common predictors
-common_protein <- intersect(rownames(weight), colnames(HGSC_prot_EVAL))
+common_protein <- intersect(rownames(weight), rownames(HGSC_prot_EVAL))
 weight <- weight[common_protein, ]
-HGSC_prot_EVAL <- HGSC_prot_EVAL[ ,common_protein]
+HGSC_prot_EVAL <- HGSC_prot_EVAL[ common_protein , ]
 
-common_feature <- intersect(colnames(weight), colnames(HGSC_rna_EVAL))
+common_feature <- intersect(colnames(weight), rownames(HGSC_rna_EVAL))
 weight <- weight[ , common_feature]
-HGSC_rna_EVAL <- HGSC_rna_EVAL[ ,common_feature]
+HGSC_rna_EVAL <- HGSC_rna_EVAL[ common_feature , ]
 
 # matrix multiplication to make the prediction
 weight <- as.matrix(weight) ; HGSC_rna_EVAL <- as.matrix(HGSC_rna_EVAL)
-prediction_ovarian <- HGSC_rna_EVAL %*% t(weight)
+prediction_ovarian <- weight %*% HGSC_rna_EVAL
 
 # save the prediction matrix
-write.csv(prediction_ovarian, paste0(path,"output/predictions.tsv")  ) 
-
+write.table(prediction_ovarian, file = paste0(path,"output/predictions.tsv"), sep="\t")
